6849893253f12aaffafaf55481129ff46a43b155
Eric Wasylishen <ewasylishen@gmail.com>
2010-06-11 19:34:05 +0000

    Opal: add a more rigorous text test
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/opal/trunk@30681 72102866-910b-0410-8b05-ffd578937521

--- Tests/texttest.m
+++ Tests/texttest.m
@@ -8,41 +8,98 @@
 
 static const char *fontName = "Times-Roman";
 
-void draw(CGContextRef currentContext, CGRect rect)
+static CGGlyph glyphs[100];
+static int advances[100];
+static CGSize sizeAdvances[100];
+static int len;
+
+/** This treats each character in str as a glyph name.
+ * It then looks up these glyph names in the given font, and stores
+ * the glyph numbers, their advances in glyph space units, 
+ * and their CGSize advances in text space units.
+ */
+void getGlyphs(CGFontRef font, CGFloat size, CFStringRef str)
 {
-    // Test CGContextSelectFont and CGContextShowTextAtPoint
-
-    CGContextSetGrayFillColor(currentContext, 0, 1);
-
-    CGContextSelectFont(currentContext, fontName, 1, kCGEncodingMacRoman);
-    CGContextShowTextAtPoint(currentContext, 40, 200, "1: M x", 6);
-
-    CGContextSelectFont(currentContext, fontName, 2, kCGEncodingMacRoman);
-    CGContextShowTextAtPoint(currentContext, 40, 150, "2: M x", 6);
-
-    CGContextSelectFont(currentContext, fontName, 3, kCGEncodingMacRoman);
-    CGContextShowTextAtPoint(currentContext, 40, 100, "3: M x", 6);
-
-    CGContextSelectFont(currentContext, fontName, 4, kCGEncodingMacRoman);
-    CGContextShowTextAtPoint(currentContext, 40, 50, "4: M x", 6);
-
-    // Test CGContextShowText
-    
-    CGContextSelectFont(currentContext, fontName, 1, kCGEncodingMacRoman);
-    CGContextShowTextAtPoint(currentContext, 40, 250, "This-", 5);    
-    CGContextShowText(currentContext, "and-that", 8);
+  len = CFStringGetLength(str);
+  for (int i=0; i<len; i++)
+  {
+    CFStringRef chr = CFStringCreateWithSubstring(NULL, str, CFRangeMake(i, 1));
+    glyphs[i] = CGFontGetGlyphWithGlyphName(font, chr);
+    CFRelease(chr);
+  }
+  CGFontGetGlyphAdvances(font, glyphs, len, advances);
   
+  CGFloat glpyhSpaceToTextSpaceFactor = 
+    (1.0f / (CGFloat)CGFontGetUnitsPerEm(font)) * (size);
+    
+  for (int i=0; i<len; i++)
+  {
+    sizeAdvances[i] = CGSizeMake(advances[i] * glpyhSpaceToTextSpaceFactor, 0);
+  }
+}
 
-    CGFontRef font = CGFontCreateWithFontName(@"Times-Roman");
-    int i;
-    for ( i=0; i<CGFontGetNumberOfGlyphs(font); i++){
-      printf("%d", i);
-      NSLog(@"%@", CGFontCopyGlyphNameForGlyph(font, i));
-    }
 
-    CGContextSetFont(currentContext, font);
-    CGContextSetFontSize(currentContext, 4);
 
-    CGGlyph glyphs[2] = {36, 57};
-    CGContextShowGlyphs(currentContext, glyphs, 2);
+void dumpGlpyhNames(CGFontRef f)
+{
+  for (int i=0; i<CGFontGetNumberOfGlyphs(f); i++){
+    CFStringRef str = CGFontCopyGlyphNameForGlyph(f, i);
+    printf("Glyph %d = '%s'\n", i, CFStringGetCStringPtr(str, kCFStringEncodingMacRoman));
+    CFRelease(str);
+  }
+}
+
+
+
+void draw(CGContextRef ctx, CGRect rect)
+{
+  // Test CGContextSelectFont and CGContextShowTextAtPoint
+
+  CGContextSetGrayFillColor(ctx, 0, 1);
+
+  CGContextSelectFont(ctx, fontName, 5, kCGEncodingMacRoman);
+  CGContextShowTextAtPoint(ctx, 10, 20, "5 pt text", 9);
+  CGContextSelectFont(ctx, fontName, 10, kCGEncodingMacRoman);
+  CGContextShowTextAtPoint(ctx, 10, 40, "10 pt text", 10);
+  CGContextSelectFont(ctx, fontName, 20, kCGEncodingMacRoman);
+  CGContextShowTextAtPoint(ctx, 10, 60, "20 pt text", 10);
+
+  // Test that the current position is being updated
+  
+  CGContextShowText(ctx, ", and some more", 15);
+
+
+  // Test CGContextSetFont / SetFontSize / CGContextShowGlyphsWithAdvances
+  
+  CGFontRef f = CGFontCreateWithFontName(CFSTR("Helvetica"));
+  CGContextSetFont(ctx, f);
+  CGContextSetFontSize(ctx, 20);
+  getGlyphs(f, 20, CFSTR("ShowGlpyhsWithAdvances"));
+  CGContextShowGlyphsWithAdvances(ctx, glyphs, sizeAdvances, len);
+
+
+  // Try some fancy glyphs
+  CGFontRef f2 = CGFontCreateWithFontName(CFSTR("Times-Roman"));
+  CGContextSetFont(ctx, f2);
+  dumpGlpyhNames(f2);
+  CGGlyph ligatures[2] = {CGFontGetGlyphWithGlyphName(f, CFSTR("fl")),
+    CGFontGetGlyphWithGlyphName(f2, CFSTR("fi"))};
+  CGContextShowGlyphsAtPoint(ctx, 10, 80, ligatures, 2);
+  
+  
+  // Test out the text matrix.
+  CGGlyph AEligatures[2] = {CGFontGetGlyphWithGlyphName(f, CFSTR("AE")),
+    CGFontGetGlyphWithGlyphName(f2, CFSTR("ae"))};
+
+  CGAffineTransform xform = CGAffineTransformIdentity;
+  xform = CGAffineTransformTranslate(xform, 10, 100);
+  CGContextSetTextMatrix(ctx, xform); // Shows that the text position is part of the text matrix
+  CGContextShowGlyphs(ctx, AEligatures, 2);
+
+  xform = CGAffineTransformIdentity;
+  xform = CGAffineTransformScale(xform, 2, 2); // Font size is 20, scaling by 2, means 40 pt text
+  xform = CGAffineTransformTranslate(xform, 5, 60);
+  CGContextSetTextMatrix(ctx, xform);
+  CGContextSetFontSize(ctx, 20); // This line should do nothing because the text matrix and font size are independent
+  CGContextShowGlyphs(ctx, AEligatures, 2);
 }
