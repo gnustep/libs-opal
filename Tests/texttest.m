95377f2dda975746615431e7a9c6f89f91a6f85d
Eric Wasylishen <ewasylishen@gmail.com>
2010-06-11 20:40:16 +0000

    Opal: Dump some info about the fonts in the text test
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/opal/trunk@30684 72102866-910b-0410-8b05-ffd578937521

--- Tests/texttest.m
+++ Tests/texttest.m
@@ -38,7 +38,12 @@ void getGlyphs(CGFontRef font, CGFloat size, CFStringRef str)
   }
 }
 
-
+static char string[256];
+char *getString(CFStringRef s)
+{
+  CFStringGetCString(s, string, 256, kCFStringEncodingUTF8);
+  return string;
+}
 
 void dumpGlpyhNames(CGFontRef f)
 {
@@ -46,13 +51,69 @@ void dumpGlpyhNames(CGFontRef f)
   printf("Dumping glpyhs for %p, %d glyphs:\n", f, nglyphs);
   for (int i=0; i<nglyphs; i++){
     CFStringRef str = CGFontCopyGlyphNameForGlyph(f, i);
-    char name[256];
-    CFStringGetCString(str, name, 256, kCFStringEncodingASCII);
-    printf("Glyph %d = '%s'\n", i, name);
+    printf("Glyph %d = '%s'\n", i, getString(str));
     CFRelease(str);
   }
 }
 
+void dumpFontInfo(CGFontRef f)
+{
+  printf("Dumping font info for %p\n", f);
+  printf("CGFontCopyFullName: %s\n", getString(CGFontCopyFullName(f)));
+  printf("CGFontCopyPostScriptName: %s\n", getString(CGFontCopyPostScriptName(f)));
+  printf("CGFontCanCreatePostScriptSubset type1: %d\n", (int)CGFontCanCreatePostScriptSubset(f, kCGFontPostScriptFormatType1));
+  printf("CGFontCanCreatePostScriptSubset type3: %d\n", (int)CGFontCanCreatePostScriptSubset(f, kCGFontPostScriptFormatType3));
+  printf("CGFontCanCreatePostScriptSubset type42: %d\n", (int)CGFontCanCreatePostScriptSubset(f, kCGFontPostScriptFormatType42));
+  printf("CGFontGetAscent: %d\n", CGFontGetAscent(f));  
+  printf("CGFontGetCapHeight: %d\n", CGFontGetCapHeight(f));  
+  printf("CGFontGetDescent: %d\n", CGFontGetDescent(f));
+  CGRect r = CGFontGetFontBBox(f);  
+  printf("CGFontGetFontBBox: ((%f,%f),(%f,%f))\n", (float)r.origin.x, (float)r.origin.y, (float)r.size.width, (float)r.size.height);
+    
+  for (int i=0; i<CGFontGetNumberOfGlyphs(f); i++)
+  {
+    CFStringRef name = CGFontCopyGlyphNameForGlyph(f, i);
+    if (i != CGFontGetGlyphWithGlyphName(f, name))
+    {
+      printf("!!! CGFontCopyGlyphNameForGlyph and CGFontGetGlyphWithGlyphName disagree on glyph %d\n", i);
+    }
+    CFRelease(name);
+  }
+  printf("CGFontGetItalicAngle: %f\n", (float)CGFontGetItalicAngle(f));  
+  printf("CGFontGetLeading: %d\n", CGFontGetLeading(f));
+  printf("CGFontGetNumberOfGlyphs: %d\n", (int)CGFontGetNumberOfGlyphs(f));
+  printf("CGFontGetStemV: %f\n", (float)CGFontGetStemV(f));  
+  printf("CGFontGetUnitsPerEm: %d\n", (int)CGFontGetUnitsPerEm(f));
+  printf("CGFontGetXHeight: %d\n", (int)CGFontGetXHeight(f));
+
+/* To test:
+CFDataRef CGFontCopyTableForTag(CGFontRef font, uint32_t tag);
+
+CFArrayRef CGFontCopyTableTags(CGFontRef font);
+
+CFArrayRef CGFontCopyVariationAxes(CGFontRef font);
+
+CFDictionaryRef CGFontCopyVariations(CGFontRef font);
+
+CGFontRef CGFontCreateCopyWithVariations(
+  CGFontRef font,
+  CFDictionaryRef variations
+);
+
+CFDataRef CGFontCreatePostScriptEncoding(
+  CGFontRef font,
+  const CGGlyph encoding[256]
+);
+
+CFDataRef CGFontCreatePostScriptSubset(
+  CGFontRef font,
+  CFStringRef name,
+  CGFontPostScriptFormat format,
+  const CGGlyph glyphs[],
+  size_t count,
+  const CGGlyph encoding[256]
+);*/
+}
 
 
 void draw(CGContextRef ctx, CGRect rect)
@@ -76,6 +137,7 @@ void draw(CGContextRef ctx, CGRect rect)
   // Test CGContextSetFont / SetFontSize / CGContextShowGlyphsWithAdvances
   
   CGFontRef f = CGFontCreateWithFontName(CFSTR("Helvetica"));
+  dumpFontInfo(f);  
   CGContextSetFont(ctx, f);
   CGContextSetFontSize(ctx, 20);
   getGlyphs(f, 20, CFSTR("ShowGlpyhsWithAdvances"));
@@ -84,6 +146,7 @@ void draw(CGContextRef ctx, CGRect rect)
 
   // Try some fancy glyphs
   CGFontRef f2 = CGFontCreateWithFontName(CFSTR("Times-Roman"));
+  dumpFontInfo(f2);
   CGContextSetFont(ctx, f2);
   dumpGlpyhNames(f2);
   CGGlyph ligatures[2] = {CGFontGetGlyphWithGlyphName(f, CFSTR("fl")),
