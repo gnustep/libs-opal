ed87ada8bb4e6423931751b26b4996f7b45897ec
Eric Wasylishen <ewasylishen@gmail.com>
2010-06-28 22:40:46 +0000

    Opal: CGImageSource skeleton implementation
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/opal/trunk@30884 72102866-910b-0410-8b05-ffd578937521

--- Source/CGImageSource.m
+++ Source/CGImageSource.m
@@ -22,6 +22,9 @@
 
 #include "CoreGraphics/CGImageSource.h"
 
+#import <Foundation/NSArray.h>
+#import <Foundation/NSSet.h>
+
 /* Constants */
 
 const CFStringRef kCGImageSourceTypeIdentifierHint = @"kCGImageSourceTypeIdentifierHint";
@@ -33,24 +36,112 @@ const CFStringRef kCGImageSourceThumbnailMaxPixelSize = @"kCGImageSourceThumbnai
 const CFStringRef kCGImageSourceCreateThumbnailWithTransform = @"kCGImageSourceCreateThumbnailWithTransform";
 
 
-@interface CGImageSource : NSObject
+static NSMutableArray *sourceClasses = nil;
 
+@interface CGImageSource : NSObject
+{
+}
+
++ (void) registerSourceClass: (Class)cls;
++ (NSArray*) sourceClasses;
++ (Class) sourceClassForType: (NSString*)type;
+
++ (NSArray *)typeIdentifiers;
++ (BOOL)canDecodeData: (CGDataProviderRef)provider;
+- (id)initWitProvider: (CGDataProviderRef)provider;
+- (NSDictionary*)propertiesWithOptions: (NSDictionary*)opts;
+- (NSDictionary*)propertiesWithOptions: (NSDictionary*)opts atIndex: (size_t)index;
+- (size_t)count;
+- (CGImageRef)createImageAtIndex: (size_t)index options: (NSDictionary*)opts;
+- (CGImageRef)createThumbnailAtIndex: (size_t)index options: (NSDictionary*)opts;
+- (CGImageSourceStatus)status;
+- (CGImageSourceStatus)statusAtIndex: (size_t)index;
+- (NSString*)type;
+- (void)updateData: (NSData*)data finalUpdate: (bool)finalUpdate;
+- (void)updateDataProvider: (CGDataProviderRef)provider finalUpdate: (bool)finalUpdate;
 
 @end
 
 @implementation CGImageSource
 
-- (id) copyWithZone: (NSZone*)zone
++ (void) registerSourceClass: (Class)cls
 {
+  if (nil == sourceClasses)
+  {
+    sourceClasses = [[NSMutableArray alloc] init];
+  }
+  [sourceClasses addObject: cls];
+}
++ (NSArray*) sourceClasses
+{
+  return sourceClasses;
 }
 
-- (void) dealloc
++ (NSArray *)typeIdentifiers
 {
-  [super dealloc];
+  [self doesNotRecognizeSelector: _cmd];
+  return nil;
+}
++ (BOOL)canDecodeData: (CGDataProviderRef)provider
+{
+  [self doesNotRecognizeSelector: _cmd];
+  return NO;
+}
+- (id)initWitProvider: (CGDataProviderRef)provider
+{
+  [self doesNotRecognizeSelector: _cmd];
+  return nil;
+}
+- (NSDictionary*)propertiesWithOptions: (NSDictionary*)opts
+{
+  [self doesNotRecognizeSelector: _cmd];
+  return nil;
+}
+- (NSDictionary*)propertiesWithOptions: (NSDictionary*)opts atIndex: (size_t)index
+{
+  [self doesNotRecognizeSelector: _cmd];
+  return nil;
+}
+- (size_t)count
+{
+  [self doesNotRecognizeSelector: _cmd];
+  return 0;
+}
+- (CGImageRef)createImageAtIndex: (size_t)index options: (NSDictionary*)opts
+{
+  [self doesNotRecognizeSelector: _cmd];
+  return nil;
+}
+- (CGImageRef)createThumbnailAtIndex: (size_t)index options: (NSDictionary*)opts
+{
+  [self doesNotRecognizeSelector: _cmd];
+  return nil;
+}
+- (CGImageSourceStatus)status
+{
+  [self doesNotRecognizeSelector: _cmd];
+  return 0;
+}
+- (CGImageSourceStatus)statusAtIndex: (size_t)index
+{
+  [self doesNotRecognizeSelector: _cmd];
+  return 0;
+}
+- (NSString*)type
+{
+  [self doesNotRecognizeSelector: _cmd];
+  return nil;
+}
+- (void)updateData: (NSData*)data finalUpdate: (bool)finalUpdate
+{
+  [self doesNotRecognizeSelector: _cmd];
+}
+- (void)updateDataProvider: (CGDataProviderRef)provider finalUpdate: (bool)finalUpdate
+{
+  [self doesNotRecognizeSelector: _cmd];
 }
 
 @end
-
 
 /* Functions */
 
@@ -58,29 +149,59 @@ const CFStringRef kCGImageSourceCreateThumbnailWithTransform = @"kCGImageSourceC
 
 CGImageSourceRef CGImageSourceCreateIncremental(CFDictionaryRef opts)
 {
-  
+  return nil;//FIXME
 }
 
 CGImageSourceRef CGImageSourceCreateWithData(
   CFDataRef data,
   CFDictionaryRef opts)
 {
-  
+  CGDataProviderRef provider = CGDataProviderCreateWithCFData(data);
+  CGImageSourceRef source;
+  source = CGImageSourceCreateWithDataProvider(provider, opts);
+  CGDataProviderRelease(provider);
+  return source;
 }
 
 CGImageSourceRef CGImageSourceCreateWithDataProvider(
   CGDataProviderRef provider,
   CFDictionaryRef opts)
 {
+  NSString *possibleType = [(NSDictionary*)opts valueForKey:
+    (NSString*)kCGImageSourceTypeIdentifierHint];
+  if (possibleType)
+  {
+    Class cls = [CGImageSource sourceClassForType: possibleType]; 
+    if ([cls canDecodeData: provider])
+    {
+      return [[cls alloc] initWithProvider: provider];
+    } 
+  }
   
+  NSUInteger cnt = [sourceClasses count];
+  for (NSUInteger i=0; i<cnt; i++)
+  {    
+    Class cls = [sourceClasses objectAtIndex: i];
+    if ([cls canDecodeData: provider])
+    {
+      return [[cls alloc] initWithProvider: provider];
+    }
+  }
+  
+  return nil;
 }
 
 CGImageSourceRef CGImageSourceCreateWithURL(
   CFURLRef url,
   CFDictionaryRef opts)
 {
-  
+  CGDataProviderRef provider = CGDataProviderCreateWithURL(url);
+  CGImageSourceRef source;
+  source = CGImageSourceCreateWithDataProvider(provider, opts);
+  CGDataProviderRelease(provider);
+  return source;
 }
+
 
 /* Accessing Properties */
 
@@ -88,7 +209,7 @@ CFDictionaryRef CGImageSourceCopyProperties(
   CGImageSourceRef source,
   CFDictionaryRef opts)
 {
-  
+  return (CFDictionaryRef)[source propertiesWithOptions: (NSDictionary*)opts];
 }
 
 CFDictionaryRef CGImageSourceCopyPropertiesAtIndex(
@@ -96,21 +217,28 @@ CFDictionaryRef CGImageSourceCopyPropertiesAtIndex(
   size_t index,
   CFDictionaryRef opts)
 {
-  
+  return (CFDictionaryRef)[source propertiesWithOptions: (NSDictionary*)opts atIndex: index];
 }
 
 /* Getting Supported Image Types */
 
 CFArrayRef CGImageSourceCopyTypeIdentifiers()
 {
-  
+  NSMutableSet *set = [NSMutableSet set];
+  NSArray *classes = [CGImageSource sourceClasses];
+  NSUInteger cnt = [classes count];
+  for (NSUInteger i=0; i<cnt; i++)
+  {    
+    [set addObjectsFromArray: [[classes objectAtIndex: i] typeIdentifiers]];
+  }
+  return (CFArrayRef)[[set allObjects] retain];
 }
 
 /* Accessing Images */
 
 size_t CGImageSourceGetCount(CGImageSourceRef source)
 {
-  
+  return [(CGImageSource*)source count];
 }
 
 CGImageRef CGImageSourceCreateImageAtIndex(
@@ -118,7 +246,7 @@ CGImageRef CGImageSourceCreateImageAtIndex(
   size_t index,
   CFDictionaryRef opts)
 {
-  
+  return [(CGImageSource*)source createImageAtIndex: index options: (NSDictionary*)opts];
 }
 
 CGImageRef CGImageSourceCreateThumbnailAtIndex(
@@ -126,24 +254,24 @@ CGImageRef CGImageSourceCreateThumbnailAtIndex(
   size_t index,
   CFDictionaryRef opts)
 {
-  
+  return [(CGImageSource*)source createThumbnailAtIndex: index options: (NSDictionary*)opts];
 }
 
 CGImageSourceStatus CGImageSourceGetStatus(CGImageSourceRef source)
 {
-  
+  return [(CGImageSource*)source status];  
 }
 
 CGImageSourceStatus CGImageSourceGetStatusAtIndex(
   CGImageSourceRef source,
   size_t index)
 {
-  
+  return [(CGImageSource*)source statusAtIndex: index];
 }
 
 CFStringRef CGImageSourceGetType(CGImageSourceRef source)
 {
-  
+  return [(CGImageSource*)source type];
 }
 
 void CGImageSourceUpdateData(
@@ -151,7 +279,7 @@ void CGImageSourceUpdateData(
   CFDataRef data,
   bool finalUpdate)
 {
-  
+  [(CGImageSource*)source updateData: (NSData*)data finalUpdate: finalUpdate];
 }
 
 void CGImageSourceUpdateDataProvider(
@@ -159,7 +287,7 @@ void CGImageSourceUpdateDataProvider(
   CGDataProviderRef provider,
   bool finalUpdate)
 {
-  
+  [(CGImageSource*)source updateDataProvider: provider finalUpdate: finalUpdate];
 }
 
 CFTypeID CGImageSourceGetTypeID()
